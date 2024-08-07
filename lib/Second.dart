46083f6e1458172f7b81177740ecfbcf41c106c2
Hafizsaqlain <saqlainurrehman0@gmail.com>
2024-08-07 21:56:46 +0500

    stackoverflow

--- lib/Second.dart
+++ lib/Second.dart
@@ -17,78 +17,80 @@ class Second extends StatelessWidget {
       backgroundColor: Color.fromARGB(255, 125, 183, 177),
       body: SafeArea(
         child: Center(
-          child: Column(
-            children:  [
-              SizedBox(height: 50,),
- 
-              Icon(
-                Icons.card_giftcard,
-                size: 100,
-              ),
-
-              SizedBox(height: 50,),
-
-              Text('Welcome To My App',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 25,
-                fontWeight: FontWeight.bold
-              
-                
-              ),),
-
-              SizedBox(height: 50,),
-
-            
-
-               SizedBox(height: 50,),
-               
+          child: SingleChildScrollView(
+            child: Column(
+              children:  [
+                SizedBox(height: 50,),
              
-
-
-               SizedBox(height: 50,),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                
-                    fillColor: Colors.white,
-                    filled: true,
-                  ),
-
+                Icon(
+                  Icons.card_giftcard,
+                  size: 100,
                 ),
+            
+                SizedBox(height: 50,),
+            
+                Text('Welcome To My App',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold
                 
-                
-              ),
-              SizedBox(height: 50,),
+                  
+                ),),
+            
+                SizedBox(height: 50,),
+            
               
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextField(decoration: InputDecoration(
-                
+            
+                 SizedBox(height: 50,),
+                 
+               
+            
+            
+                 SizedBox(height: 50,),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextField(
+                    decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.black),
                       ),
+                      
                       focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.black),
                       ),
                   
                       fillColor: Colors.white,
                       filled: true,
-                    ),),
-                    
-              ),
-             
-              
-            ],
+                    ),
             
+                  ),
+                  
+                  
+                ),
+                SizedBox(height: 50,),
+                
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextField(decoration: InputDecoration(
+                  
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                    
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),),
+                      
+                ),
+               
+                
+              ],
+              
+            ),
           ),
           
         ),
